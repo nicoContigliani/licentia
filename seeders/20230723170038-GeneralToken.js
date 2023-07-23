7a33c94877987de3c4daecd45b0da158e27fa80c
nicoContigliani <nico.contigliani@gmail.com>
2023-07-23 14:06:34 -0300

    todo

--- seeders/20230723170038-GeneralToken.js
+++ seeders/20230723170038-GeneralToken.js
@@ -1,25 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('GeneralTokens', [
+      { LicenceId: 1, Token_Value: 'jwt-token-general-1', Expiration_Date: new Date(), Revoked: false, Revoked_At: null, IP_Address: '192.168.1.1', customer: 'Customer 1', messages: 'Message 1', alerts: 'Alert 1', createdAt: new Date(), updatedAt: new Date() },
+      { LicenceId: 2, Token_Value: 'jwt-token-general-2', Expiration_Date: new Date(), Revoked: false, Revoked_At: null, IP_Address: '192.168.1.2', customer: 'Customer 2', messages: 'Message 2', alerts: 'Alert 2', createdAt: new Date(), updatedAt: new Date() }
+      // Agrega más registros según tus necesidades
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('GeneralTokens', null, {});
   }
 };
