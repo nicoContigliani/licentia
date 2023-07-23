4090b61264fdc42595479eabed3995b200188d10
nicoContigliani <nico.contigliani@gmail.com>
2023-07-23 13:39:36 -0300

    seder

--- seeders/20230723140725-LicenceTypes.js
+++ seeders/20230723140725-LicenceTypes.js
@@ -1,25 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('LicenceTypes', [
+      { Type_Name: 'Type 1', Description: 'Description for Type 1', Max_Duration: 365, Premium_Support: true, Users_Included: 10, createdAt: new Date(), updatedAt: new Date() },
+      { Type_Name: 'Type 2', Description: 'Description for Type 2', Max_Duration: 180, Premium_Support: false, Users_Included: 5, createdAt: new Date(), updatedAt: new Date() }
+      // Agrega más registros según tus necesidades
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('LicenceTypes', null, {});
   }
 };
