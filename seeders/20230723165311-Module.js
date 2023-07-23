e96abb1117edc3fac3b49e146e9db8a26ffb2ed5
nicoContigliani <nico.contigliani@gmail.com>
2023-07-23 14:04:56 -0300

    seed module types

--- seeders/20230723165311-Module.js
+++ seeders/20230723165311-Module.js
@@ -1,25 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Modules', [
+      { Module_Name: 'Module 1', Module_Description: 'Description for Module 1', Module_TypeID: 1, createdAt: new Date(), updatedAt: new Date() },
+      { Module_Name: 'Module 2', Module_Description: 'Description for Module 2', Module_TypeID: 2, createdAt: new Date(), updatedAt: new Date() }
+      // Agrega más registros según tus necesidades
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Modules', null, {});
   }
 };
--- seeders/20230723165313-ModuleType.js
+++ seeders/20230723165313-ModuleType.js
@@ -1,25 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('ModuleTypes', [
+      { Type_Name: 'Type 1', Description: 'Description for Type 1', createdAt: new Date(), updatedAt: new Date() },
+      { Type_Name: 'Type 2', Description: 'Description for Type 2', createdAt: new Date(), updatedAt: new Date() }
+      // Agrega más registros según tus necesidades
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('ModuleTypes', null, {});
   }
 };
