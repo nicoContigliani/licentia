4007d074302fdda08110ac3362f23f85e309e767
nicoContigliani <nico.contigliani@gmail.com>
2023-07-26 16:36:16 -0300

    todo

--- src/services/jwt.services.ts
+++ src/services/jwt.services.ts
@@ -3,35 +3,7 @@ import 'dotenv/config'
 
 
 export const jwtGenerateToken = async (data: any) => {
-    const token = jwt.sign(data, process.env.JWT_SIGNATURE_GENERAL, { expiresIn: 3600 });
-
-    // interface IdataPersonObject {
-    //     firstname: string,
-    //     mail: string,
-    //     password: string
-    // }
-
-    // const dataUser: IdataPersonObject[] = [];
-
-    // const firstname: string = "nico";
-    // const mail: string = "nico.contigliani@gmai.com"
-    // const password: string = "123456789";
-    // const isUser: boolean = true;
-    // const fullname: string = "nicolasContigliani";
-    // const numberGetin: number = 15;
-    // dataUser.push({
-    //     firstname,
-    //     mail,
-    //     password
-    // })
-    // console.log("🚀 ~ file: jwt.services.ts:27 ~ jwtGenerateToken ~ dataUser:", dataUser)
-
-
-
-    return token
-
-
-
+    return jwt.sign(data, process.env.JWT_SIGNATURE_GENERAL, { expiresIn: 3600 });
 }
 
 export const jwtCompareToken = async () => {
